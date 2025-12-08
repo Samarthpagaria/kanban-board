f8bfc39a627a3b749cc4809a3ae81f0af4160d75
pagariasamarth@gmail.com <pagariasamarth@gmail.com>
2025-12-08 11:55:38 +0530

    Add new task functionality done and delete functionality done

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 const todo = document.querySelector("#todo");
 const progress = document.querySelector("#progress");
-
+const addNewTaskBtn = document.querySelector("#toggle-modal");
+const modal = document.querySelector(".modal");
 const done = document.querySelector("#done");
+const addTaskBtn = document.querySelector("#add-task-btn");
+
 console.log(todo, progress, done);
 
+let dragElement = null;
 const task = document.querySelectorAll(".task");
 
 task.forEach((task) =>
   task.addEventListener("drag", (e) => {
     // console.log("dragging");
+    dragElement = task;
   })
 );
 
+addNewTaskBtn.addEventListener("click", () => {
+  modal.classList.toggle("active");
+});
+
+addTaskBtn.addEventListener("click", () => {
+  const taskTitle = document.querySelector("input").value;
+  const taskDescription = document.querySelector("textarea").value;
+  const task = document.createElement("div");
+  task.classList.add("task");
+  const titleEl = document.createElement("h2");
+  titleEl.textContent = taskTitle;
+  const descriptionEl = document.createElement("p");
+  descriptionEl.textContent = taskDescription;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("button");
+
+  task.appendChild(titleEl);
+  task.appendChild(descriptionEl);
+  task.appendChild(deleteBtn);
+  todo.appendChild(task);
+  modal.classList.remove("active");
+  document.querySelector("input").value = "";
+  document.querySelector("textarea").value = "";
+});
 function addDragEventsOnColumn(column) {
   column.addEventListener("dragenter", (e) => {
     e.preventDefault();
@@ -26,8 +57,17 @@ function addDragEventsOnColumn(column) {
   });
   column.addEventListener("drop", (e) => {
     e.preventDefault();
+    column.appendChild(dragElement);
+    dragElement = null;
+    column.classList.remove("hover-over");
   });
 }
 addDragEventsOnColumn(todo);
 addDragEventsOnColumn(progress);
 addDragEventsOnColumn(done);
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("button")) {
+    e.target.parentElement.remove();
+  }
+});
